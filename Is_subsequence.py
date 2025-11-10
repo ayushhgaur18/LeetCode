ae4f459d2fc132f65e2af943879aeadcc811f1a5
Ayush Gaur <ayushgaur18@gmail.com>
2025-11-10 11:07:33 +0530

    Is_subsequence solved

---
+++ Is_subsequence.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        ptr_s = ptr_t = 0
+        while ptr_s < len(s) and ptr_t < len(t):
+            if s[ptr_s] == t[ptr_t]:
+                ptr_s += 1
+            ptr_t += 1
+        return ptr_s >= len(s)
